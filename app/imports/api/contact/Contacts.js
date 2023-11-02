28c04c8ef1b09f2bc2abe572d96e4da2977e33db
Ethan Mamuad <ethantm@hawaii.edu>
2023-11-01 20:25:45 -1000

    contacts collection finished in 30 minutes.

--- app/imports/api/contact/Contacts.js
+++ app/imports/api/contact/Contacts.js
@@ -17,6 +17,8 @@ class ContactsCollection {
       address: String,
       image: String,
       description: String,
+      owner: String,
+    });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
     // Define names for publications and subscriptions
