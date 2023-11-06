595c11b6dfbbaf65fb55b470157919243c45bf17
Ethan Mamuad <ethantm@hawaii.edu>
2023-11-05 20:46:55 -1000

    Revert "notes-1 finished in 45 minutes."
    This reverts commit e5c12dd3dab2bacf63a3784d3c93572eda580fb5.

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -3,27 +3,23 @@ import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Contacts } from '../../api/contact/Contacts';
-import { Notes } from '../../api/note/Notes';
 import Contact from '../components/Contact';
 import LoadingSpinner from '../components/LoadingSpinner';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const ListContacts = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, contacts, notes } = useTracker(() => {
+  const { ready, contacts } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
     const subscription = Meteor.subscribe(Contacts.userPublicationName);
-    const subscription2 = Meteor.subscribe(Notes.userPublicationName);
     // Determine if the subscription is ready
-    const rdy = subscription.ready() && subscription2.ready();
+    const rdy = subscription.ready();
     // Get the Stuff documents
     const contactItems = Contacts.collection.find({}).fetch();
-    const noteItems = Notes.collection.find({}).fetch();
     return {
       contacts: contactItems,
-      notes: noteItems,
       ready: rdy,
     };
   }, []);
@@ -36,7 +32,7 @@ const ListContacts = () => {
             <h2 style={{ color: 'white' }}>List Contacts</h2>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
-            {contacts.map((contact) => (<Col key={contact.id}><Contact contact={contact} notes={notes.filter(note => (note.contactId === contact._id))} /></Col>))}
+            {contacts.map((contact) => (<Col key={contact.id}><Contact contact={contact} /></Col>))}
           </Row>
         </Col>
       </Row>
